0b39dccbad04a037032ce61eca620ae123ec9883
JoseLopezLara <jose.lopez.lara.cto@gmail.com>
2025-10-12 19:03:57 -0600

    I've inited this project with: JPA, spring security, mysql driver, spring WEB, spring dev tools and lombook

---
+++ src/main/java/dev/englishapi/english_api/EnglishApiApplication.java
@@ -0,0 +1,13 @@
+package dev.englishapi.english_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishApiApplication.class, args);
+	}
+
+}
